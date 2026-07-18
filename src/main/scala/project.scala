d45a1ef4d45c3a070bfac75f8728edf14c047d57
ConicalDrupe <ChrisJacobBoon@gmail.com>
2026-07-18 18:25:02 -0500

    correctly convert to scala-cli

--- src/main/scala/project.scala
+++ src/main/scala/project.scala
@@ -1,4 +1,4 @@
 //> using scala "3.8.3"
-//> using dep "org.monovore::decline:2.6.1"
+//> using dep "com.monovore::decline:2.6.1"
 //> using test.dep "org.scalatest::scalatest:3.2.17"
 //> using options -Wunused:all -deprecation
