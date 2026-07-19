35d2f388e4740d75a37e91dbe98c0cbce0d0bd83
ConicalDrupe <ChrisJacobBoon@gmail.com>
2026-07-18 21:11:40 -0500

    reduced redundant optional, renamed app

--- src/main/MneumosApp.scala
+++ src/main/MneumosApp.scala
@@ -3,10 +3,9 @@ import cats.syntax.all._
 
 // We'll start by defining our individual options...
 val modeOpt = Opts
-  .option[String](
-    "mode", short = "m",
-    metavar ="mode_type",
-  help="Run mode of cli. (note) by default, there is also run,report"
+  .argument[String](
+    "mode",
+  // help="Run mode of cli. (note) by default, there is also run,report"
   )
   .withDefault("note")
 val tagOpt = Opts
@@ -18,21 +17,21 @@ val palaceOpt = Opts.option[String](
 
 // ...along with a case class that captures all our configuration data.
 sealed trait InputConfig
-case class MemoryConfig(tag: String, palace: String) extends InputConfig
+case class MemoryConfig(tag: Option[String], palace: Option[String]) extends InputConfig
 case class RunConfig(mode: String) //note, run, report
     extends InputConfig
 
 case class Config(
     runConfig: RunConfig,
-    memoryConfig: Option[MemoryConfig]
+    memoryConfig: MemoryConfig
 )
 
 val runOpt = (modeOpt).map(RunConfig.apply)
-val memoryOpt = (tagOpt, palaceOpt).mapN(MemoryConfig.apply)
-val configOpts: Opts[Config] = (runOpt, memoryOpt.orNone).mapN(Config.apply)
+val memoryOpt = (tagOpt.orNone, palaceOpt.orNone).mapN(MemoryConfig.apply)
+val configOpts: Opts[Config] = (runOpt, memoryOpt).mapN(Config.apply)
 
 // Where does validation come in?
-object App extends CommandApp (name = "mem",
+object MneumosApp extends CommandApp (name = "mem",
   header = "interact with mnemos-cli",
   main = configOpts.map { config => println(s"running with config $config")},
   version = "0.0.x"
