46238200922bd7af0677a31dc30798ccce2d7457
ConicalDrupe <ChrisJacobBoon@gmail.com>
2026-07-20 16:41:51 -0500

    Added doobie-core and sqlite jdbc

--- src/main/scala/project.scala
+++ src/main/scala/project.scala
@@ -1,4 +1,5 @@
 //> using scala "3.8.3"
-//> using dep "com.monovore::decline:2.6.1"
-//> using test.dep "org.scalatest::scalatest:3.2.17"
+//> using dep "com.monovore::decline:2.6.2"
+//> using dep "org.typelevel::doobie-core:1.0.0-RC13"
+//> using dep "org.xerial:sqlite-jdbc:3.53.2.0"
 //> using options -Wunused:all -deprecation
